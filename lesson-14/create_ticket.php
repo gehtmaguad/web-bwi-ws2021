aa10e63954f6bb6c2bd0d1f7bb8ccf47231ce857
Markus Hoesel <hoesel@technikum-wien.at>
2021-12-10 16:13:24 +0100

    add exercises for 14th class

---
+++ lesson-14/create_ticket.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+    <title>Pic-Uploader and DB</title>
+</head>
+<body>
+<div class="container">
+    <form action="" id="ticketform" method="POST" enctype="multipart/form-data">
+        <div class="col">
+            <textarea rows="4" cols="50" name="comment" form="ticketform" class="form-control" id="comment" placeholder="Comment"></textarea>
+        </div>
+        <div class="col">
+            <div class="input-group">
+                <div class="custom-file">
+                        <input class="custom-file-input" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" type="file" name="picture" required accept="image/jpeg, image/png, image/gif">
+                        <label class="custom-file-label" for="inputGroupFile04">Choose file</label>
+                </div>
+                <button class="btn btn-outline-secondary" id="inputGroupFileAddon04" type="submit" name="submit">Upload</button>
+                </form>
+            </div>
+
+        <?php
+            $uid = 127; // get it from the $_SESSION
+            $comment = "empty";
+            $date = new DateTime();
+            $timestamp = $date->getTimestamp();
+            $target_dir = 'pics/';
+            $file = @$_FILES["picture"];
+            $picname = explode(".", @$_FILES["picture"]["name"]);
+            $target_file = $target_dir . $picname[0] . "_". $timestamp . "." . end($picname);
+            //$target_file = $target_dir . $timestamp . "_". basename(@$_FILES["picture"]["name"]);
+            $uploadStatus = 1;
+            $acceptedtype = ["jpg", "jpeg", "png", "gif"];
+
+            if (isset($_POST["submit"])) {
+                if (isset($_POST["comment"]) && !empty($_POST["comment"])) {
+                    $comment = $_POST["comment"];
+                }
+
+                // Check type
+                $uploadExt = strtolower(pathinfo($_FILES['picture']['name'], PATHINFO_EXTENSION));
+                if (!in_array($uploadExt, $acceptedtype)) {
+                    echo "<div class='red'>Sorry, only image-files can be accepted!</div>";
+                    $uploadStatus = 0;
+                }
+
+                // Check file size
+                if ($file["size"] > 15000000) {
+                    echo "<div class='red'>Sorry, your file is too big! The maximum file size is 15MB!</div>";
+                    $uploadStatus = 0;
+                }
+
+                // Check if file already exists
+                if (file_exists($target_file)) {
+                    echo "<div class='red'>Sorry, this file already exists!</div>";
+                    $uploadStatus = 0;
+                }
+
+                echo $file["tmp_name"];
+
+                // Check if $uploadStatus is 0
+                if ($uploadStatus == 0) {
+                    echo "Please try again.<br>";
+                }
+
+                // If everything is OK, upload the file
+                else {
+                    if (move_uploaded_file($file["tmp_name"], $target_file)) {
+                        createDBentry($comment, $target_file, $uid);
+                    } else {
+                        echo "<div class='red'>Sorry, something went wrong during the upload.</div>";
+                    }
+                }
+            }
+
+            function createDBentry($c, $path, $user_id) {
+                require_once ('dbaccess.php');
+
+                $db_obj = new mysqli($host, $user, $password, $database);
+                $sql = "INSERT INTO `tickets` (`file_path`, `comment`, `user_id`) VALUES (?, ?, ?)";
+                $stmt = $db_obj->prepare($sql);
+                $stmt-> bind_param("ssi", $path, $c, $user_id);
+
+                if ($stmt->execute()) {
+                    echo "<div class='green'>The picture has been uploaded.</div>";
+                }
+                else {
+                    echo "<div class='red'>Sorry, something went wrong during the upload.</div>";
+                }
+                $stmt->close();
+                $db_obj->close();
+            }
+
+        ?>
+    </div>
+</div>
+</body>
+</html>
---
+++ lesson-14/show_all_tickets.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <title>All tickets</title>
+</head>
+<body>
+
+    <?php
+        require_once ('dbaccess.php');
+
+        $db_obj = new mysqli($host, $user, $password, $database);
+        if ($db_obj->connect_error) {
+            echo "Connection Error: " . $db_obj->connect_error;
+            exit();
+        }
+
+        $sql = "SELECT * FROM tickets";
+        $stmt = $db_obj->prepare($sql);
+        $stmt->execute();
+        $stmt->bind_result($ticketID, $file_path, $comment, $user_id);
+    ?>
+
+    <h1>Tickets</h1>
+    <table border="1">
+        <th>Ticket ID</th>
+        <th>User ID</th>
+        <th>Comment</th>
+        <th>Picture</th>
+        <th>File path</th>
+
+        <?php
+            while ($stmt->fetch()) {
+                echo "<tr>";
+                echo "<td>" . $ticketID . "</td>";
+                echo "<td>" . $user_id . "</td>";
+                echo "<td>" . $comment . "</td>";
+                echo "<td><img src='$file_path' alt='picture' width='200'></td>";
+                echo "<td><a href='" . $file_path . "' target='_blank'>Full resolution</a>". "</td>";
+                echo "</tr>";
+            }
+            $stmt->close();
+            $db_obj->close();
+        ?>
+    </table>
+
+</body>
+</html>
